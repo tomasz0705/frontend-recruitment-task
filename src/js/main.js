88651ef0262b466d2180d128e170f48cea75c929
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-06-07 23:32:11 +0200

    Implemented saving count of clicks to localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,43 @@ const closeBtn = document.querySelector(".closer");
 const resetBtn = document.querySelector(".resetButton");
 const counter = document.getElementById("clicks");
 
+const saveToLocalStorage = (key, value) => localStorage.setItem(key, value);
+
+const getItemFromLocalStorage = (key) => localStorage.getItem(key);
+
+const NUMBER_OF_CLICKS_LS_KEY = "number-of-clicks";
+
+const getClicksCounterFromLocalStorage = () =>
+  Number(getItemFromLocalStorage(NUMBER_OF_CLICKS_LS_KEY)) || 0;
+
 btn.addEventListener("click", (event) => {
-    event.preventDefault();
-    popUp.style.display = "flex";
+  event.preventDefault();
+  popUp.style.display = "flex";
+
+  const currentClick = getClicksCounterFromLocalStorage() + 1;
+
+  localStorage.setItem(NUMBER_OF_CLICKS_LS_KEY, currentClick);
+
+  counter.innerHTML = currentClick;
+
+  if (currentClick > 5) {
+    resetBtn.style.display = "flex";
+
+    resetBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+      saveToLocalStorage(NUMBER_OF_CLICKS_LS_KEY, null);
+      counter.innerHTML = 0;
+    });
+  }
 });
 
 closeBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    popUp.style.display = "none";
+  event.preventDefault();
+  popUp.style.display = "none";
 });
 
 window.addEventListener("click", (event) => {
-    if(event.target.id === "popup") {
-        popUp.style.display = "none";
-    }
+  if (event.target.id === "popup") {
+    popUp.style.display = "none";
+  }
 });
